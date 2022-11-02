876b4e414bbff9f5c76f97260f2741d2f422ac12
Bartek <czarny.bm@gmail.com>
2022-11-02 20:54:58 +0100

    Drugie szkolenie z JS

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Bartek');
+var firstName = "Bartek Musialski jest the best";
+var theWord = " Jedno przykładowe zdanie   ";
+
+console.log(firstName);
+
+console.log(firstName.toLowerCase());
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.slice(0,6));
+console.log(firstName.split(' '));
+console.log(firstName.split('jest'));
+
+console.log(firstName.length);
+console.log(theWord.trim().length);
+console.log(theWord.startsWith(' '))
